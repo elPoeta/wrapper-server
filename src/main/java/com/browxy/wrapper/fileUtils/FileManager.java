5a2ba43d9683eb463693897cf060ba78aaa845df
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2025-01-21 08:23:45 -0300

    add file manager to read files

---
+++ src/main/java/com/browxy/wrapper/fileUtils/FileManager.java
@@ -0,0 +1,37 @@
+package com.browxy.wrapper.fileUtils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileManager {
+
+	public static String readFile(String completeFilePath) throws FileNotFoundException {
+        return readFile(completeFilePath, "UTF-8");
+    }
+
+    public static String readFile(String fileName, String encoding) throws FileNotFoundException {
+        return readFile(new FileInputStream(fileName), encoding);
+    }
+
+    public static String readFile(InputStream inputStream, String encoding) {
+        return readFile(inputStream, encoding, true);
+    }
+
+    public static String readFile(InputStream inputStream, String encoding, boolean preserveCR) {
+        StringBuilder strBuilder = null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, encoding))) {
+            strBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                strBuilder.append(line + (preserveCR ? '\n' : ""));
+            }
+        } catch (Exception e1) {
+            throw new RuntimeException(e1);
+        }
+        return strBuilder.toString();
+    }
+}
+
